719c96b066d3aa5cbc35d5524308ca57166e8113
Jallouli-Yassine <86846631+Jallouli-Yassine@users.noreply.github.com>
2022-08-23 14:09:16 +0100

    Update mail.php

--- CourseManagment/controllers/mail.php
+++ CourseManagment/controllers/mail.php
@@ -23,8 +23,8 @@ $mail = new PHPMailer();
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'jungloul1@gmail.com';                  //SMTP username
-    $mail->Password   = 'azaazaA123456789';                     //SMTP password
+    $mail->Username   = '';                  //SMTP username
+    $mail->Password   = '';                     //SMTP password
     $mail->SMTPSecure = 'ssl';                                  //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
